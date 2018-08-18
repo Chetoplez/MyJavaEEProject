c40e2d6fb352f13a4a80024bf15f9e80f3980718
Chetoplez <frusso.d1989@gmail.com>
2018-08-18 17:38:23 +0200

    added some dependency

--- chetoplezApp/src/main/java/webservices/ServiceTestImplementations.java
+++ chetoplezApp/src/main/java/webservices/ServiceTestImplementations.java
@@ -1,4 +1,5 @@
 package webservices;
 
+
 public class ServiceTestImplementations {
 }
--- chetoplezApp/src/main/java/webservices/interfaces/ControllerInteface.java
+++ chetoplezApp/src/main/java/webservices/interfaces/ControllerInteface.java
@@ -1,8 +1,10 @@
 package webservices.interfaces;
 
+import org.springframework.stereotype.Service;
 import webservices.model.HelloRequest;
 import webservices.model.HelloResponse;
 
+@Service
 public interface ControllerInteface {
     public HelloResponse helloWorld(HelloRequest request);
 }
