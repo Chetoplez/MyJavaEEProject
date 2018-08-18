319d5aa9ef01bf0d700630ec642cdd10e65d093c
Chetoplez <frusso.d1989@gmail.com>
2018-08-18 16:46:57 +0200

    added lombok dependency

--- chetoplezApp/src/main/java/webservices/model/ResponseObject.java
+++ chetoplezApp/src/main/java/webservices/model/ResponseObject.java
@@ -1,5 +1,15 @@
-package webservices.model;
+package chetoplezApp.src.main.java.webservices.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseObject {
-
+    public static enum EXIT_CODE {
+        OK, KO
+    }
+    private String reason;
+    private String addictionalMessage;
+    private EXIT_CODE exitCode;
 }
